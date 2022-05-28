d953f61e7a4a553d722cc8b31733315973596505
nickgrief <wotniga@gmail.com>
2022-05-29 02:36:17 +0300

    Create elements of the grid

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("working");
+// Big box with boxes in a column with boxes inside them in a row. (big brain)
+
+const body = document.querySelector("body");
+
+// Make grid
+const grid = document.createElement("div");
+
+// Make horizontal holders
+for (let i = 0; i < 16; i++) {
+  let row = document.createElement("div");
+  for (let j = 0; j < 16; j++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
+  row.classList.add("row");
+  grid.appendChild(row);
+}
+
+// Add grid to page (do it after all styling)
+grid.classList.add("grid");
+body.appendChild(grid);
