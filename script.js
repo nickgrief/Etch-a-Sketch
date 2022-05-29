71c13489dd34b56af60e633579be86f0e091b910
nickgrief <wotniga@gmail.com>
2022-05-29 03:54:34 +0300

    Add hower effect

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.addEventListener("mouseenter", () => {
+      cell.classList.toggle("howered");
+    });
     row.appendChild(cell);
   }
   row.classList.add("row");
